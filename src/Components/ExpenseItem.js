27e3492c07ab5c75248e017d84163440b304147c
epi.ds <essidsleheddine@gmail.com>
2022-10-11 16:53:28 +0200

    pushed

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React , {useState} from "react";
 import "./ExpenseItem.css";
 import ExpenceDate from "./ExpenceDate";
 function ExpenseItem(props) {
-  console.log("from item",props.props.props[0])
+  let [title,setTitle] = useState(props.props.props[0].title)
   const expenseTitle = props.props.props[0].title;
   const expensePrice = props.props.props[0].price;
   console.log("exoence price",expensePrice)
+  const click=()=>{ 
+    setTitle("new title");
+    }
+    console.log(title)
   return (
     <div className="expense-item">
       <div>
-
         <ExpenceDate props={props.props.props[0].date}/>
-                </div>
+      </div>
       <div className="expense-item__description ">
         <h2>{expenseTitle}</h2>
       </div>
       <div className="expense-item__price">{expensePrice}</div>
+      <button onClick={click} >yessssss</button>
     </div>
   );
 }
--- src/Components/Expenses.js
+++ src/Components/Expenses.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import ExpenseItem from './ExpenseItem'
+import Card from './Card'
 import  "./Expences.css"
 function Expenses(props) {
     
   return (
+    <Card>
+
     <div>
         <ExpenseItem className='expences' props={props}/>
-
+        <ExpenseItem className='expences' props={props}/>
+        <ExpenseItem className='expences' props={props}/>
     </div>
+
+
+    </Card>
   )
 }
 
